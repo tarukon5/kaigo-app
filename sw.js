21edb9925efb031f73e8483d3fd090fe37e87bca
tarukon5 <tarukon5@users.noreply.github.com>
2026-05-05 08:51:59 +0900

    Initial commit: おぼえて帳 PWA

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'kaigo-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/kaigo-app/'])).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+    if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+    return res;
+  })));
+});
